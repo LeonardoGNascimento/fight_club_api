9ceb2f50bdac7501b2076cb67c2c61c9957f3f81
LeonardoGNascimento <leonardogabrielllpedroso@gmail.com>
2025-02-08 11:37:46 -0300

    tyresdyr

--- src/precos/precos.controller.ts
+++ src/precos/precos.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { PrecosService } from './precos.service';
+import { Public } from 'src/_core/decorator/public.decorator';
 
 @Controller('precos')
 export class PrecosController {
   constructor(private service: PrecosService) {}
 
   @Get()
+  @Public()
   findAll(@Query() filtros: any) {
     return this.service.findAll(filtros);
   }
