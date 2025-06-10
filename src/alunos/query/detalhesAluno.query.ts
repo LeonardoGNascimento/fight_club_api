2e633b470317f5d44d59b3f8ead28a17fad0aac8
LeonardoGNascimento <leonardogabrielllpedroso@gmail.com>
2025-06-09 22:49:46 -0300

    ajustes

--- src/alunos/query/detalhesAluno.query.ts
+++ src/alunos/query/detalhesAluno.query.ts
@@ -15,18 +15,24 @@ interface Modalidades extends AlunosModalidades {
   historico: AlunosGraducaoHistorico[];
 }
 
-export class DetalhesAlunosQuery {
-  id: string;
-  dataHora: Date;
-  nome: string;
+export class DetalhesAlunosQuery extends Alunos {
   plano: Planos;
   modalidades: Modalidades[];
 
   constructor(data?: Alunos) {
+    super();
+
     if (data) {
       this.id = data.id;
       this.dataHora = data.dataHora;
       this.nome = data.nome;
+      this.cpf = data.cpf;
+      this.telefone = data.telefone;
+      this.cep = data.cep;
+      this.cidade = data.cidade;
+      this.estado = data.estado;
+      this.rua = data.rua;
+      this.numero = data.numero;
 
       if (data.alunosModalidades) {
         this.setModalidades(data.alunosModalidades);
